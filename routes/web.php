6209d1d25fbf4779fc7019bdfbcc7115d0a4678d
Mariano Arias <marianoariasdev@gmail.com>
2024-04-09 12:32:45 -0300

    Routes Added

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\LinkController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -14,4 +15,7 @@ Route::middleware([
     Route::get('/dashboard', function () {
         return view('dashboard');
     })->name('dashboard');
+
+    Route::resource('links', LinkController::class)->names('links');
+    Route::get('/links/{link}', [LinkController::class, 'visit'])->name('links.visit');
 });
